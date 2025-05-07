865f091742fab9cf231dcff80d75e7fc3b16a5ab
unknown <ashishkumawat685@gmail.com>
2025-05-07 15:13:11 +0530

    Updated API for POST users

--- Schema/Connect.js
+++ Schema/Connect.js
@@ -15,3 +15,25 @@ mongoose
   .catch(() => {
     console.log("Cannot connect mongoose with  mongodb");
   });
+
+// const mongoose = require("mongoose");
+
+let isConnected;
+
+const connectdata = async () => {
+  if (isConnected) return;
+
+  try {
+    const db = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    isConnected = db.connections[0].readyState;
+    console.log("✅ MongoDB connected");
+  } catch (err) {
+    console.error("❌ MongoDB connection failed:", err);
+    throw err;
+  }
+};
+
+module.exports = connectdata;
